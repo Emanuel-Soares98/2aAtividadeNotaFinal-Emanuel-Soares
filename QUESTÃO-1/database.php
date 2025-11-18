4f75c77ac9190595258cc54c70dacbedc0bea9e5
Emanuel-Soares98 <soaresemanuel98@gmail.com>
2025-11-18 19:49:10 -0300

    Create database.php

---
+++ QUESTÃO-1/database.php
@@ -0,0 +1,14 @@
+<?php
+$db = new PDO('sqlite:books.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+$db->exec("
+    CREATE TABLE IF NOT EXISTS books (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        author TEXT NOT NULL,
+        year INTEGER NOT NULL
+    )
+");
+?>
