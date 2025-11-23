63f325e5efadef2fccfbebdf578c7660dd80f247
Emanuel-Soares98 <soaresemanuel98@gmail.com>
2025-11-23 00:01:55 -0300

    Implement task completion update in PHP

--- QUESTÃO-2/update_tarefa.php
+++ QUESTÃO-2/update_tarefa.php
@@ -1,1 +1,12 @@
-teste
+<?php
+include 'database.php';
+
+$idTarefa = $_GET['id'];
+
+$stmt = $conexao->prepare("UPDATE tarefas SET concluida = 1 WHERE id = ?");
+$stmt->bind_param("i", $idTarefa);
+$stmt->execute();
+
+header("Location: index.php");
+exit;
+?>
