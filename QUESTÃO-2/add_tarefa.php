2691981666cd0cc415b58608a25368d09ac810e8
Emanuel-Soares98 <soaresemanuel98@gmail.com>
2025-11-22 23:58:54 -0300

    Update add_tarefa.php

--- QUESTÃO-2/add_tarefa.php
+++ QUESTÃO-2/add_tarefa.php
@@ -1,1 +1,13 @@
-teste
+<?php
+include 'database.php';
+
+$descricao = $_POST['descricao'];
+$dataVencimento = $_POST['data_vencimento'];
+
+$stmt = $conexao->prepare("INSERT INTO tarefas (descricao, data_vencimento) VALUES (?, ?)");
+$stmt->bind_param("ss", $descricao, $dataVencimento);
+$stmt->execute();
+
+header("Location: index.php");
+exit;
+?>
